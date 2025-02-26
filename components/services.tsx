d5bf888a99d4ed8f2c3bf2a29668635ec6a066c1
Oyebamiji Alex <alexferanmi390@gmail.com>
2025-02-26 12:28:29 +0100

    chore: refactored services component

--- components/services.tsx
+++ components/services.tsx
@@ -2,12 +2,7 @@
 
 import { motion } from "framer-motion";
 import ServiceCard from "@/components/service-card";
-import speed from "@/public/images/speed.svg";
-import document from "@/public/images/document.svg";
-import message from "@/public/images/message.svg";
-import scan from "@/public/images/scan.svg";
-import people from "@/public/images/people.svg";
-import sms from "@/public/images/sms.svg";
+import { serviceA, serviceB } from "@/utils/constant";
 
 export default function Services() {
     const text = "Unlocking Potential, Together";
@@ -53,39 +48,25 @@ export default function Services() {
                 <section className="mt-10 xl:mt-[100px]">
                     <article
                         className="flex flex-wrap items-center justify-between gap-10 xl:flex-nowrap">
-                        <ServiceCard
-                            icon={speed}
-                            title="High-Speed Internet"
-                            subtitle="Experience reliable, lightning-fast internet for seamless productivity"
-                        />
-                        <ServiceCard
-                            icon={document}
-                            title="Meeting Rooms"
-                            subtitle="Bookable meeting rooms equipped with AV facilities for presentations and conferences."
-                        />
-                        <ServiceCard
-                            icon={scan}
-                            title="Printing and Scanning"
-                            subtitle="On-site printing, scanning, and copying services for documents."
-                        />
+                        {serviceA.map((service) => (
+                            <ServiceCard
+                                key={service.id}
+                                icon={service.icon}
+                                title={service.title}
+                                subtitle={service.subtitle}
+                            />
+                        ))}
                     </article>
                     <article
-                        className="mt-10 flex flex-wrap items-center justify-between gap-10 xl:flex-nowrap">
-                        <ServiceCard
-                            icon={message}
-                            title="Tech Support"
-                            subtitle="Basic IT support for troubleshooting connectivity or equipment issues"
-                        />
-                        <ServiceCard
-                            icon={people}
-                            title="Networking Opportunities"
-                            subtitle="Introductions to potential collaborators, mentors, or investors within the coworking community."
-                        />
-                        <ServiceCard
-                            icon={sms}
-                            title="Mail Handling"
-                            subtitle="Reception services for mail and packages, with secure storage for members."
-                        />
+                        className="mt-12 flex flex-wrap items-center justify-between gap-10 xl:flex-nowrap">
+                        {serviceB.map((service) => (
+                            <ServiceCard
+                                key={service.id}
+                                icon={service.icon}
+                                title={service.title}
+                                subtitle={service.subtitle}
+                            />
+                        ))}
                     </article>
                 </section>
             </div>
--- utils/constant.ts
+++ utils/constant.ts
@@ -1,5 +1,66 @@
-export const menu = [
+import speed from "@/public/images/speed.svg";
+import document from "@/public/images/document.svg";
+import message from "@/public/images/message.svg";
+import scan from "@/public/images/scan.svg";
+import people from "@/public/images/people.svg";
+import sms from "@/public/images/sms.svg";
+
+interface serviceProps {
+    id: number;
+    title: string;
+    icon: string;
+    subtitle: string;
+}
+
+interface menu {
+    id: number;
+    name: string;
+}
+
+export const menu: menu[] = [
     {id: 1, name: "Home"},
     {id: 2, name: "About"},
     {id: 3, name: "Membership"}
 ];
+
+export const serviceA: serviceProps[] = [
+    {
+        id: 1,
+        icon: speed,
+        title: "High-Speed Internet",
+        subtitle: "Experience reliable, lightning-fast internet for seamless productivity."
+    },
+    {
+        id: 2,
+        icon: document,
+        title: "Meeting Rooms",
+        subtitle: "Bookable meeting rooms equipped with AV facilities for presentations and conferences."
+    },
+    {
+        id: 3,
+        icon: scan,
+        title: "Printing and Scanning",
+        subtitle: "On-site printing, scanning, and copying services for documents."
+    }
+]
+
+export const serviceB: serviceProps[] = [
+    {
+        id: 1,
+        icon: message,
+        title: "Tech Support",
+        subtitle: "Basic IT support for troubleshooting connectivity or equipment issues."
+    },
+    {
+        id: 2,
+        icon: people,
+        title: "Networking Opportunities",
+        subtitle: "Introductions to potential collaborators, mentors, or investors within the coworking community."
+    },
+    {
+        id: 3,
+        icon: sms,
+        title: "Mail Handling",
+        subtitle: "Reception services for mail and packages, with secure storage for members."
+    }
+]
